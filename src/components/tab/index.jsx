541d842a2b92070657d790f211a2ff4793037354
keunyeong <91kyeong@gmail.com>
2022-05-08 17:53:45 +0900

    Feat: Tab 컴포넌트 완료.

--- src/components/tab/index.jsx
+++ src/components/tab/index.jsx
@@ -3,15 +3,46 @@ import { useState } from 'react'
 import classnames from 'classnames'
 // eslint-disable-next-line react/prop-types
 export default function Tab() {
+  const [slider, setSlider] = useState(0)
+  const handleMenuClick = (e) => {
+    const { num } = e.currentTarget.dataset
+    setSlider(Number(num))
+  }
   return (
     <div className={styles.tab}>
       <div className={styles.menuBox}>
-        <div className={styles.menuText}>HOME</div>
-        <div className={styles.menuText}>SCHOOL</div>
-        <div className={styles.menuText}>STORE</div>
-      </div>
-      <div className={styles.menuSliderBox}>
-        <div className={styles.menuSlider} />
+        <div
+          data-num='0'
+          className={classnames(styles.menuText, { [styles.selected]: slider === 0 })}
+          onClick={handleMenuClick}
+          aria-hidden
+        >
+          HOME
+        </div>
+        <div
+          data-num='1'
+          className={classnames(styles.menuText, { [styles.selected]: slider === 1 })}
+          onClick={handleMenuClick}
+          aria-hidden
+        >
+          SCHOOL
+        </div>
+        <div
+          data-num='2'
+          className={classnames(styles.menuText, { [styles.selected]: slider === 2 })}
+          onClick={handleMenuClick}
+          aria-hidden
+        >
+          STORE
+        </div>
+        <div
+          className={classnames(
+            styles.menuSlider,
+            { [styles.slider2]: slider === 2 },
+            { [styles.slider1]: slider === 1 },
+            { [styles.slider0]: slider === 0 }
+          )}
+        />
       </div>
     </div>
   )
